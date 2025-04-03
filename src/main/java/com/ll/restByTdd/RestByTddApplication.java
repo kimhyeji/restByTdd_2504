a4fddad1b19b81b9aaa7f8935c4123941273a168
kimhyeji <gpwl5589@gmail.com>
2025-04-03 23:19:59 +0900

    setting 2

--- src/main/java/com/ll/restByTdd/RestByTddApplication.java
+++ src/main/java/com/ll/restByTdd/RestByTddApplication.java
@@ -2,8 +2,10 @@ package com.ll.restByTdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
+@ActiveProfiles("test")
 public class RestByTddApplication {
 
 	public static void main(String[] args) {
