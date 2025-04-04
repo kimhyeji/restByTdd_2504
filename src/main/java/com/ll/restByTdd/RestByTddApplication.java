43ce10b576ff0944b943a61c0d4622b90301758f
kimhyeji <gpwl5589@gmail.com>
2025-04-04 22:17:32 +0900

    base work

--- src/main/java/com/ll/restByTdd/RestByTddApplication.java
+++ src/main/java/com/ll/restByTdd/RestByTddApplication.java
@@ -2,10 +2,10 @@ package com.ll.restByTdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@ActiveProfiles("test")
+@EnableJpaAuditing
 public class RestByTddApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ll/restByTdd/domain/member/member/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.ll.restByTdd.domain.member.member.service;
+
+import com.ll.restByTdd.domain.member.member.entity.Member;
+import com.ll.restByTdd.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public long count() {
+        return memberRepository.count();
+    }
+
+    public Member join(String username, String password, String nickname) {
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .nickname(nickname)
+                .apiKey(UUID.randomUUID().toString())
+                .build();
+
+        return memberRepository.save(member);
+    }
+
+    public Optional<Member> findById(long authorId) {
+        return memberRepository.findById(authorId);
+    }
+
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    public Optional<Member> findByApiKey(String apiKey) {
+        return memberRepository.findByApiKey(apiKey);
+    }
+}
